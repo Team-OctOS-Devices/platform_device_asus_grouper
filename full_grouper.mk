9964ca18a0e5922361a3ce0fb89411bbde45c0ba
hedwig34 <hedwig34@gmail.com>
2015-08-25 14:21:04 -0400

    grouper: include octos vendor config
    Change-Id: I64b00d7e0f757ce620fca7404783227481c571eb

--- full_grouper.mk
+++ full_grouper.mk
@@ -25,8 +25,12 @@ $(call inherit-product, device/asus/grouper/device.mk)
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
+# Include OctOs common configuration
+include vendor/to/config/main.mk
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_grouper
 PRODUCT_DEVICE := grouper
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Grouper
+PRODUCT_RESTRICT_VENDOR_FILES := false
